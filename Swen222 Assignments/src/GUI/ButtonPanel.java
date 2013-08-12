dd6f7826e0075f4d94721db00be65f491297d148
Emmanuel Godinez <manuel.godinez22@gmail.com>
2013-08-12 19:50:26 +1200

    Huwwo... this is kinda fun :P

--- Swen222 Assignments/src/GUI/ButtonPanel.java
+++ Swen222 Assignments/src/GUI/ButtonPanel.java
@@ -1,7 +1,13 @@
 package GUI;
 
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class ButtonPanel extends JPanel{
 	
+	public ButtonPanel(){
+		this.setSize(WIDTH, 100);
+		this.setBackground(Color.DARK_GRAY);
+	}
 }
--- Swen222 Assignments/src/GUI/GamePanel.java
+++ Swen222 Assignments/src/GUI/GamePanel.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -10,6 +11,7 @@ public class GamePanel extends JPanel{
 	public void paint(Graphics gr){
 		Graphics2D g = (Graphics2D)gr.create();
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
+		g.setColor(Color.LIGHT_GRAY);
+		g.fill3DRect(0, 0, WIDTH, HEIGHT, true);
 	}
 }
