f2f3a7bcb347bddd817668b07c590c789afabae4
Emmanuel Godinez <manuel.godinez22@gmail.com>
2013-08-12 19:46:33 +1200

    another commit

--- Swen222 Assignments/src/GUI/GamePanel.java
+++ Swen222 Assignments/src/GUI/GamePanel.java
@@ -1,7 +1,15 @@
 package GUI;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import javax.swing.*;
 
 public class GamePanel extends JPanel{
-
+	public void paint(Graphics gr){
+		Graphics2D g = (Graphics2D)gr.create();
+		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+	}
 }
--- Swen222 Assignments/src/GUI/MainFrame.java
+++ Swen222 Assignments/src/GUI/MainFrame.java
@@ -16,22 +16,19 @@ public class MainFrame extends JFrame{
 		this.host = host;
 		
 		initialiseFrame();
-		
-		this.add(gp);
-		this.add(bp, BorderLayout.SOUTH);
-		
-		
-		
-		
 	}
 
 	private void initialiseFrame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 650);
+		this.setLocation(10, 10);
 		this.setResizable(false);
 		this.setLayout(new BorderLayout());
 		this.gp = new GamePanel();
 		this.bp = new ButtonPanel();
+		this.add(gp);
+		this.add(bp, BorderLayout.SOUTH);
+
 		this.show();
 	}
 }
